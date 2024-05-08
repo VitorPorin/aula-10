004844d6161747d717ff89f54b5cd1406dfd9b3a
Porin <162372947+VitorPorin@users.noreply.github.com>
2024-05-08 12:02:00 -0300

    commit construtor

---
+++ aula10/src/Relogio.java
@@ -0,0 +1,61 @@
+public class Relogio {
+   private int hora;
+   private int minuto;
+   private int segundo;
+//construtor valor
+   public Relogio() {
+
+
+   }  
+
+// Tradicional (inserindo todos os atributos)
+    public Relogio(int hora, int minuto, int segundo){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+}
+
+// 2ª versão: Inserindo apenas hora e minuto
+    public Relogio(int hora, int minuto){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo =0;
+    }
+    //3ª Versão: Inserindo apenas a hora
+    public Relogio (int hora){
+        this.hora = hora;
+        this.minuto=0;
+        this.segundo=0;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+
+
+
+
+
+
+    
+}
